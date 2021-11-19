518c6a0bc373fabb50c6ed7ca75d400f2c5b8eaa
Tyler G Alexander <tylergeorgealexander@gmail.com>
2021-11-19 08:27:26 -0800

    Added handlers, useEffect Fetches, and map etc

--- src/Components/PokeCard.js
+++ src/Components/PokeCard.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from "react";
-import { Route, Routes, useParams } from "react-router-dom";
+import { Route, Routes, useParams, useHistory, Link } from "react-router-dom";
 
 export default function PokeCard() {
   const { pokeId } = useParams();
+  const history = useHistory();
 
+  const [index, setIndex] = useState([]);
   // Used this url for name
   const url = `https://pokeapi.co/api/v2/pokemon/${pokeId}`;
   // Used this url for description
@@ -18,21 +20,43 @@ export default function PokeCard() {
   useEffect(() => {
     fetch(url)
       .then((response) => response.json())
-      .then((data) => setPokemon(capitalizeFirstLetter(data.name)));
-  }, []);
+      .then((data) => setPokemon(capitalizeFirstLetter(data.name)))
+  }, [url]);
+
+  // Pokemon ID
+  useEffect(() => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => setIndex(data.id));
+  }, [url]);
 
   // Pokemon Characteristics Description
   useEffect(() => {
     fetch(urlDesc)
       .then((response) => response.json())
-      .then((data) => setDesc(data.descriptions[0].description));
-  }, []);
+      .then((data) => setDesc(data.descriptions));
+  }, [urlDesc]);
 
-
-// Make sure to invoke this on a then chain or async await
+  // Make sure to invoke this on a then chain or async await
   function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
   }
+
+  // Mapping different languages of description
+  let descMap = "Loading...";
+  if (desc) {
+    descMap = desc.map((d) => {
+      return <li>{d.description}</li>;
+    });
+  }
+
+  const nextHandler = async () => {
+    history.push(`/${index + 1}`)
+  };
+
+  const previousHandler = async () => {
+    history.push(`/${index - 1}`)
+  };
 
   // TEST AREA FOR CONSOLE LOG
   console.log(desc);
@@ -49,12 +73,14 @@ export default function PokeCard() {
 
         <div className="card-body">
           <h5 className="card-title">{pokemon}</h5>
-          <p className="card-text">
-            {`Description: ${desc}`}
-          </p>
-          <a href="#" className="btn btn-primary">
-            Go somewhere
-          </a>
+          <ul className="card-text">{descMap}</ul>
+          <p>{index}</p>
+          <button onClick={nextHandler} className="btn btn-primary">
+            Next
+          </button>
+          <button onClick={previousHandler} className="btn btn-primary">
+            Previous
+          </button>
         </div>
       </div>
     </div>
